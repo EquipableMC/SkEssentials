76b356b892ee1636d6a067142c9b22e411af1f3a
EquipableMC <66171067+EquipableMC@users.noreply.github.com>
2024-08-16 12:36:12 -0400

    Requested Changes

--- src/main/java/site/equipable/skEssentials/skript/expressions/ExprNickPlayers.java
+++ src/main/java/site/equipable/skEssentials/skript/expressions/ExprNickPlayers.java
@@ -1,6 +1,10 @@
 package site.equipable.skEssentials.skript.expressions;
 
 import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -14,10 +18,15 @@ import org.jetbrains.annotations.Nullable;
 import site.equipable.skEssentials.SkEssentials;
 
 
+
+@Name("Nick Players")
+@Description("Set the essentials nickname of a player.\n NOTE: You MUST specify essentials for it to work through Essentials.")
+@Examples({"set the nickname of player to \"I am a cool person!\""})
+@Since("1.0.0")
 public class ExprNickPlayers extends SimplePropertyExpression<Player, String> {
 
     static {
-        register(ExprNickPlayers.class, String.class, "(essentials[x]) [:full] nick[name]","players");
+        register(ExprNickPlayers.class, String.class, "essentials[x] [:full] nick[name]", "players");
     }
 
     private boolean fullNick;
@@ -27,8 +36,17 @@ public class ExprNickPlayers extends SimplePropertyExpression<Player, String> {
         return super.init(exprs, matchedPattern, isDelayed, parseResult);
     }
 
+    @Override
+    public @Nullable String convert(Player player) {
+        User user = SkEssentials.essentials.getUser(player);
+        if (user != null) {
+            return fullNick ? user.getNick() : user.getNickname();
+        }
+        return null;
+    }
+
     public Class<?>[] acceptChange(ChangeMode mode) {
-        if (mode == ChangeMode.SET || mode == ChangeMode.RESET) {
+        if (mode == ChangeMode.SET || mode == ChangeMode.RESET && !fullNick) {
             return CollectionUtils.array(String.class);
         }
         return null;
@@ -36,35 +54,21 @@ public class ExprNickPlayers extends SimplePropertyExpression<Player, String> {
 
     public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
         String nickName = delta != null ? (String) delta[0] : null;
-        for (Object object : getExpr().getArray(event)) {
-            if (object instanceof Player player) {
-                User user = SkEssentials.essentials.getUser(player);
-                if (user != null) {
-                    user.setNickname(nickName);
-                }
+        for (Player player : getExpr().getArray(event)) {
+            User user = SkEssentials.essentials.getUser(player);
+            if (user != null) {
+                user.setNickname(nickName);
             }
         }
-    }
-
-    @Override
-    protected String getPropertyName() {
-        return "";
-    }
-
-    @Override
-    public @Nullable String convert(Player player) {
-        User user = SkEssentials.essentials.getUser(player);
-        if (user != null) {
-            if (fullNick) {
-                return user.getNick();
-            }
-            return user.getNickname();
-        }
-        return null;
     }
 
     @Override
     public Class<? extends String> getReturnType() {
         return String.class;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "nickname";
     }
 }
